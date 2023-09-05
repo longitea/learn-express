8ebe4d7480038977e7ee9d524e5fc666d15da8db
Mona Lisa <longnt.en@gmail.com>
2023-09-05 11:03:13 +0700

    add siderbar api

--- index.js
+++ index.js
@@ -17,15 +17,14 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN,
 })
 
-
-
 app.get('/', async (req, res) => {
-    res.send('<h1>Gọi API bằng Express</h1>')
+    res.send('<h1>Gọi API bằng Express</h1>');
+
+    console.log(123);
 
 })
 
-
-// notion sdk
+// GET ALL DB API
 app.get('/NotionWiki', async (req, res) => {
     try {
         const response = await notion.databases.query({
@@ -46,4 +45,96 @@ app.get('/NotionWiki', async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+});
+
+
+// GET SIDERBAR
+app.get('/siderbar', async (req, res) => {
+    const response = await notion.databases.query({
+        database_id: process.env.NOTION_DATABASE_ID,
+        sorts: [
+            {
+                property: "Siderbar_Number",
+                direction: "ascending"
+            }
+        ],
+
+    });
+
+    // return array of result
+    const { results } = response;
+    let secondArray = []
+
+    let dbID = ''
+    results.forEach(databaseID => {
+        if (databaseID.parent.type == "database_id") {
+            let pageTitle = databaseID.properties.Page.title[0].plain_text
+            secondArray.push({
+                id: databaseID.id,
+                name: pageTitle,
+                subName: []
+            })
+        }
+    })
+
+    results.forEach(pageID => {
+        if (pageID.parent.type == "page_id") {
+            let pageTitle = pageID.properties.Page.title[0].plain_text
+            secondArray.forEach(arrayID => {
+                if (arrayID.id == pageID.parent.page_id) {
+                    arrayID.subName.push({
+                        id: pageID.id,
+                        name: pageTitle,
+                        secondSubName: []
+                    })
+                }
+            })
+        }
+    })
+
+    results.forEach(element => {
+        if (element.parent.type == "page_id") {
+            let pageTitle = element.properties.Page.title[0].plain_text
+            secondArray.forEach(arrayID => {
+                if (arrayID.subName.length != 0) {
+                    arrayID.subName.forEach(el => {
+                        if (el.id == element.parent.page_id) {
+                            el.secondSubName.push({
+                                id: element.id,
+                                name: pageTitle,
+                                thirdSubName: []
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+
+    results.forEach(element => {
+        if (element.parent.type == "page_id") {
+            let pageTitle = element.properties.Page.title[0].plain_text
+            secondArray.forEach(arrayID => {
+                if (arrayID.subName.length != 0) {
+                    arrayID.subName.forEach(el => {
+                        if (el.secondSubName.length != 0) {
+                            el.secondSubName.forEach(third => {
+                                if (third.id == element.parent.page_id) {
+                                    third.thirdSubName.push({
+                                        id: element.id,
+                                        name: pageTitle,
+                                        fourSubName: []
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+
+
+    res.send(secondArray)
+
 });
